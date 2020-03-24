33f5ff53db7cefe012ca4acc2f226317ee251f24
Ridha <rbenrhouma2@gmail.com>
2020-03-25 00:43:21 +0100

    myAutoCompleat

--- src/components/GoodLocalisation.js
+++ src/components/GoodLocalisation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CountrySelect from "./CountrySelect";
 import PostalCode from "./PostalCode";
+import MTAutocomplete from "./MT/MTAutocomplete";
 import "./style.css";
 
 import "./style.css";
@@ -11,6 +12,21 @@ const GoodLocalisation = props => {
     <div className="goodLocalisation">
       <CountrySelect></CountrySelect>
       <PostalCode></PostalCode>
+
+      <MTAutocomplete
+        suggestions={[
+          "Alligator",
+          "Bask",
+          "Crocodilian",
+          "Death Roll",
+          "Eggs",
+          "Jaws",
+          "Reptile",
+          "Solitary",
+          "Tail",
+          "Wetlands"
+        ]}
+      />
     </div>
   );
 };
